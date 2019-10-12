d0ca4cb187a57bb9f68d703d364e36cd25596007
Rebecca Bartels <rebeccakbartels@gmail.com>
2019-10-12 11:15:16 -0400

    Update inventory.py
    smushed it into a lil func

--- inventory.py
+++ inventory.py
@@ -1,17 +1,19 @@
-inventory = {}
+def begin():
+    inventory = {}
 
-item_count = int(input("How many items do you have in your inventory? "))
-for x in range(item_count):
-    item_name = input("What is the item? ")
-    item_price = input("What is the price? ")
-    inventory[item_name] = float(item_price)
-    print("--------")
+    item_count = int(input("How many items do you have in your inventory? "))
+    for x in range(item_count):
+        item_name = input("What is the item? ")
+        item_price = input("What is the price? ")
+        inventory[item_name] = float(item_price)
+        print("--------")
 
-for item, price in inventory.items():
-    print("The price of " + item + "is $" + str(price))
+    for item, price in inventory.items():
+        print("The price of " + item + "is $" + str(price))
 
-    if (price < 5):
-        print("this item is on sale!")
+        if (price < 5):
+            print("this item is on sale!")
+            print("--------")
 
 
-print("--------")
+begin()
